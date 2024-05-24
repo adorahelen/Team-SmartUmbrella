a487705b4ca3635cfa071333d3756e367e289337
adorahelen <adorahelenmin@gmail.com>
2024-05-24 22:38:03 +0900

    안될 이유가 없는데 ,,,

--- NJbrellaBack.py
+++ NJbrellaBack.py
@@ -23,43 +23,15 @@ db.init_app(app)
 def index():
     return send_from_directory('build', 'index.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
-    email = request.form['email']
-    password = request.form['password']
+    return send_from_directory('build', 'Login.js')
 
-    # 여기서는 간단히 이메일과 패스워드를 출력하는 예시입니다.
-    print(f"Received email: {email}, password: {password}")
 
-    # 로그인 성공 여부에 따라 응답을 반환할 수도 있습니다.
-    # 여기서는 간단히 성공 메시지를 반환합니다.
-    return jsonify({"message": "Login successful!"})
-
-@app.route('/signup', methods=['POST'])
+@app.route('/signup')
 def signup():
-    # POST 요청에서 데이터 추출
-    data = request.json
-
-    # 데이터 출력
-    print(data)
-
-    # 여기서는 간단히 성공 메시지를 반환
-    return jsonify({"message": "Signup successful!"})
-
-try:
-    with conn.cursor() as cursor:
-        # 간단한 SELECT 쿼리 실행
-        cursor.execute("SELECT 1")
-        result = cursor.fetchone()
-        print("Database connection is successful!" if result else "Database connection failed!")
-finally:
-    # 연결 종료
-    conn.close()
-
+    # Signup.js 파일을 제공하도록 수정
+    return send_from_directory('build', 'Signup.js')
 
 
 if __name__ == '__main__':
