0a2f0b40db69ed41eba17b9cd47fbc22d01cabb4
adorahelen <adorahelenmin@gmail.com>
2024-05-25 17:37:12 +0900

    sign(), sign2() + vs는 자동이 아니라 재빌드

--- NJbrellaBack.py
+++ NJbrellaBack.py
@@ -22,16 +22,38 @@ db.init_app(app)
 @app.route('/')
 def index():
     return send_from_directory('build', 'index.html')
-
 @app.route('/login')
 def login():
     return send_from_directory('build', 'index.html')
-
-
 @app.route('/signup')
 def signup():
-    # Signup.js 파일을 제공하도록 수정
     return send_from_directory('build', 'index.html')
+
+# 여기까지는 홈페이지를 제공하는 라우팅 함수
+
+
+@app.route('/signup', methods=['POST'])
+def signup2():
+    data = request.get_json()
+
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    email = data.get('email')
+    password = data.get('password')
+
+    print(f"Received signup data: {data}")
+
+    # 회원가입 성공 응답
+    response = {
+        'message': 'User registered successfully',
+        'user': {
+            'firstName': first_name,
+            'lastName': last_name,
+            'email': email,
+        }
+    }
+
+    return jsonify(response), 201
 
 
 if __name__ == '__main__':
